2d2f092764117434bf7517fcb06a558eaa326052
sinturana <sinturana250@gmail.com>
2023-05-10 19:11:25 +0530

    A product can be available on multiple shops and Price of a product will be according to the Brand

--- app/models.py
+++ app/models.py
@@ -9,12 +9,12 @@ from django.db import models
 5. Each Product can have different Rating according to Brand
 '''
 
-class Market(models.Model):                  #market , shop, product
+class Market(models.Model):                 #market , shop, product
     name=models.CharField(max_length=50)
 
 class Shop(models.Model):
     name=models.CharField(max_length=50)
-    market=models.ForeignKey(Market, on_delete=models.CASCADE)
+    market=models.ForeignKey(Market, on_delete=models.CASCADE, related_name='shops')
 
 
 class Brand(models.Model):
@@ -22,15 +22,16 @@ class Brand(models.Model):
 
 class Product(models.Model):
     name=models.CharField(max_length=50)
-    shop=models.ForeignKey(Shop, on_delete=models.CASCADE)
+    shop=models.ForeignKey(Shop, on_delete=models.CASCADE,related_name='products')
     price=models.DecimalField(max_digits=10, decimal_places=2)
-    brands = models.ManyToManyField(Brand)
+    brands=models.ManyToManyField(Brand, related_name='products')
+    rate = models.DecimalField(max_digits=10, decimal_places=2)
 
 
-class Rating(models.Model):
-    value=models.IntegerField()
-    brand=models.ForeignKey(Brand, on_delete=models.CASCADE)
-    product=models.ForeignKey(Product, on_delete=models.CASCADE)
+# class Rating(models.Model):
+#     value=models.IntegerField()
+#     brand=models.ForeignKey(Brand, on_delete=models.CASCADE)
+#     product=models.ForeignKey(Product, on_delete=models.CASCADE)
 
 
 
